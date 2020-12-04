d8530dd8126d458e87b5834f952b3af48c80a836
JOSEPH LARKAI <larkai.joseph@gmail.com>
2020-12-04 22:16:06 +0000

    changed icons try

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,19 +54,19 @@ class Navbar extends Component {
 
                     {/* For contact me icon in nav-bar */}
                     <div className="nav-icons-big-screen">
-                        <a href="https://www.google.com/search?pws=0&q=facebook+react-icons" className="nav-btn-big-screen">
+                        <a href="#" className="nav-btn-big-screen">
                             <button type="button">
-                                <AiOutlineInstagram className="Nav-icon" />   
+                                <i className="fas fa-cart-plus" />   
                             </button>
                         </a>
-                        <a href="https://www.google.com/search?pws=0&q=facebook+react-icons" className="nav-btn-big-screen">
+                        <a href="#" className="nav-btn-big-screen">
                             <button type="button">
-                                <AiOutlineLinkedin className="Nav-icon" />   
+                                <i className="fas fa-cart-plus" />    
                             </button>
                         </a>
-                        <a href="https://www.google.com/search?pws=0&q=facebook+react-icons" className="nav-btn-big-screen">
-                            <button type="button">
-                                <AiOutlineFacebook className="Nav-icon" />   
+                        <a href="#" className="nav-btn-big-screen">
+                            <button type="button Nav-icon">
+                                <i className="fas fa-cart-plus" />   
                             </button>
                         </a>
                     </div>
@@ -78,75 +78,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-
-
-
-
-
-
-
-
-
-
-
-// --------------------------------------------------------------
-
-
-
-// import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import jl_logo from '../logo.jpg';
-// import styled from 'styled-components';
-
-// export default class Navbar extends Component{
-
-
-//     render() {
-//         return (
-//             <div className="navbar-Wrapper">
-//                 <NavWrapper className="navbar my-navbar navbar-expand-sm px-sm-5">
-
-//                     {/* This is for the logo, bootstrap is mainly used to handle styling */}
-//                     <Link to='/'>
-//                         <img src={jl_logo} alt='Joseph Larkai Logo' className='Navbar-brand'/>
-//                     </Link>
-
-//                     {/* This Shows The 'element' text in the Nav Bar */}
-//                     <ul className="nav-links justify-content-center">
-//                         <li className="nav-item">
-//                             <Link to="/resume" className="nav-link">
-//                                 Resume.
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item">
-//                             <Link to="/projects" className="nav-link">
-//                                 Projects.
-//                             </Link>
-//                         </li>
-//                     </ul>
-
-
-//                     {/* This is for the cart */}
-//                     <Link to="/contact" className="ml-auto mail-button">
-//                         <div className="nav-icon">
-//                             <span className='mr-2'>
-//                                 <i className="fa fa-envelope-o" aria-hidden="true"/>
-//                             </span>
-//                         </div>
-//                     </Link>
-
-//                 </NavWrapper>
-//             </div>
-                        
-//         );
-//     }
-// }
-
-// // styling for Nav Bar using styled components
-// const NavWrapper = styled.nav`
-// .nav-link{
-//     color:var(--primaryColor) !important;
-//     font-size: 0.95rem;
-//     text-transform: capitalize !important;
-// }
-// `
